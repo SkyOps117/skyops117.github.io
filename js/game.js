355294e765962ec98d48e10e594c55d4a36520e2
Enkyia <minepro@live.ca>
2023-07-11 15:05:11 -0400

    Manual-commit

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
 "use strict"
-import * as BABYLON from "./node_modules/babylonjs/babylon.js";
+//import * as BABYLON from "babylonjs/babylon.js";
 import World from "./world.js";
 const world = new World();
